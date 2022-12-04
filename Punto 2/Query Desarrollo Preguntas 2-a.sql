d7c44427e09a14f690a510e5601def2e6e67144e
Roberto Vargas <87739037+Basti529@users.noreply.github.com>
2022-12-04 17:35:40 -0300

    Delete Query Desarrollo Preguntas 2-a.sql

--- Punto 2/Query Desarrollo Preguntas 2-a.sql
+++
@@ -1,9 +0,0 @@
---a.	Top de empresas que sus trabajadores presentan más licencias.
-	select   top 1
-			 a.idEmpresa
-			,b.nombre
-			,COUNT(*) as 'TopCantdadLicencias'
-	from [Licencia] a inner join [Empresa] b
-		on a.idEmpresa = b.idEmpresa
-	group by a.idEmpresa, b.nombre
-	order by COUNT(*) desc
